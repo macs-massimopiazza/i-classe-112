b4a363ab18b1d49ba7f1c90da71714d2e4eabbfc
Cristopher Ferrari <126168713+CrisFerrari1993@users.noreply.github.com>
2024-03-04 17:15:06 +0100

    Update App.vue
    Try to add somethign

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@ export default {
       frasi: [
         { testo: "fare il debugging è come essere il detective in un film giallo in cui tu sei anche l'assassino (o forse è stato lo stagista)", nome: "Massimo" },
         { testo: "non è vero, l'assassino è sempre il gatto del maggiordomo", nome: "Massimolaltro" },
+        { test: "Quando una cosa ti va male, rifalla 21 volte", nome: 'Cristopher'},
       ],
     };
   },
